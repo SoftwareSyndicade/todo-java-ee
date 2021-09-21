251184327a18e4422ab18ef881925e494c8732c5
Bibek Saini <itsbibeksaini@gmail.com>
2021-09-22 00:40:07 +0530

    Stage page and no folder illustration

---
+++ src/main/java/com/todo/servlets/TodoFolder.java
@@ -0,0 +1,37 @@
+package com.todo.servlets;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "TodoFolder", value = "/folder")
+public class TodoFolder extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>test</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,13 +4,32 @@
 <head>
     <title>JSP - Hello World</title>
     <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link href="styles/styles.css" rel="stylesheet">
+    <link href="styles/index.css" rel="stylesheet">
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
+    <div class="container-fluid max-supported-width p-3 h-100">
+        <div class="stage">
+            <div class="text-center no-folder-wrapper">
+                <img src="assets/empty.svg">
+                <h3>No todo folder found.</h3>
+                <button class="btn btn-primary" onclick="createFolder()">Create folder</button>
+            </div>
+        </div>
+    </div>
+
     <script src="scripts/jquery-3.6.0.js"></script>
     <script src="scripts/bootstrap.min.js"></script>
+    <script>
+        function createFolder(){
+            $.ajax({
+                url:'/todo/folder',
+                method: 'POST',
+                success: function (data){
+
+                }
+            })
+        }
+    </script>
 </body>
 </html>
